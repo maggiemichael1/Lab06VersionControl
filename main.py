a790d9e40d33a10fc4c53f294101c1d960f0017b
maggiemichael1 <149456884+maggiemichael1@users.noreply.github.com>
2023-10-31 18:37:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,16 +20,28 @@ def encode(password):
         dig += 3
         newPass += str(dig)
     return newPass
+
+def decode(password):
+    global oldPass
+    oldPass = ""
+    for dig in password:
+        dif = int(dig)
+        dif -= 3
+        oldPass += str(dig)
+    return oldPass
+
 def main():
     while again == True:
         printMenu()
         if userIn == "1":
+            global userIn2
             userIn2 = input("Please enter your password to encode: ")
             print("Your password has been encoded and stored!")
             encode(userIn2)
 
         elif userIn == "2":
-            print(f"The encoded password is {newPass}, and the original password is {userIn2}.")
+            decode(userIn2)
+            print(f"The encoded password is {newPass}, and the original password is {oldPass}.")
         else:
             exit()
 
